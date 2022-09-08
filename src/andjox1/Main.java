2d2d69397206f3e4299f4a48bdbd01d154b49662
trumanders <trumanders@gmail.com>
2022-09-09 01:40:59 +0200

    igen

--- src/andjox1/Main.java
+++ src/andjox1/Main.java
@@ -8,6 +8,7 @@ public class Main {
     private static void showMenu() {
         System.out.println("BANK\n----");
         System.out.println("1. List customers");
+        System.out.println("2. Another option");
         System.out.println("0. Quit");
     }
 }
